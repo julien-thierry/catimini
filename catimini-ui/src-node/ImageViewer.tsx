e6c5761019d4e2de68525fbae6dd059451dff2e2
Julien Thierry <julienthierry3110@gmail.com>
2025-07-09 23:07:10 +0200

    [feature] catimini-ui/ImageViewer: display first image from available list when starting the application

--- catimini-ui/src-node/ImageViewer.tsx
+++ catimini-ui/src-node/ImageViewer.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
+import "./ImageViewer.css";
+
 
 function ImageViewer() {
     const [imageList, setImageList] = useState<Array<String>>([]);
@@ -16,8 +18,28 @@ function ImageViewer() {
             .catch(e => console.warn("Failed to retrieve images"));
     }, []);
 
+    const [imageData, setImageData] = useState<ArrayBuffer | null>(null);
+
+    async function fetchImage(imgPath) : Promise<ArrayBuffer> {
+        console.debug("Fetching image: ", imgPath);
+        return await invoke("fetch_image", { path : imgPath});
+    }
+    useEffect(() => {
+        if (imageList.length > 0) {
+            fetchImage(imageList[0])
+                .then((value) => setImageData(value.byteLength > 0 ? value : null))
+                .catch((e) => setImageData(null));
+        } else {
+            setImageData(null);
+        }
+    }, [imageList]);
+
+    const imgBlob =  imageData != null ? new Blob([imageData]) : null;
+    const imgURL = imgBlob != null ? URL.createObjectURL(imgBlob) : null;
+
     return (
         <div className="imageview">
+            {imgURL != null ? (<img src={imgURL} alt="my image"/>) : (<></>)}
         </div>
     );
 }
