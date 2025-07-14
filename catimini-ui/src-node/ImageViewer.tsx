f97a46391a848e3491c6c129c1e47443b479fc87
Julien Thierry <julienthierry3110@gmail.com>
2025-07-15 00:30:48 +0200

    [feature] catimini-ui: Add previous and next image buttons to the image view

--- catimini-ui/src-node/ImageViewer.tsx
+++ catimini-ui/src-node/ImageViewer.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Button } from "react-bootstrap";
+import { FaCaretLeft, FaCaretRight } from "react-icons/fa";
 import { invoke } from "@tauri-apps/api/core";
 import "./ImageViewer.css";
 
@@ -19,27 +21,58 @@ function ImageViewer() {
     }, []);
 
     const [imageData, setImageData] = useState<ArrayBuffer | null>(null);
+    const [imageIdx, setImageIdx] = useState(-1);
+    const currImagePathRef = useRef<String | null>(null);
+
+    const updateImageIdx = useCallback((newIdx) => {
+        setImageIdx(newIdx);
+        currImagePathRef.current = newIdx >= 0 ? imageList[newIdx] : null;
+    }, [imageList]);
+
+    useEffect(() => {
+        let newIdx = -1;
+        if (currImagePathRef.current != null) {
+            // lookup old path
+            newIdx = imageList.indexOf(currImagePathRef.current);
+        } else if (imageList.length > 0) {
+            // TODO: If workspace changed completely we want to go back to the begining
+            // if the current item was deleted, we want to go to the next one.
+            // For now, go back to first element in the list.
+            newIdx = 0;
+        }
+        updateImageIdx(newIdx);
+    }, [imageList]);
 
     async function fetchImage(imgPath) : Promise<ArrayBuffer> {
         console.debug("Fetching image: ", imgPath);
         return await invoke("fetch_image", { path : imgPath});
     }
     useEffect(() => {
-        if (imageList.length > 0) {
-            fetchImage(imageList[0])
+        if (currImagePathRef.current != null) {
+            fetchImage(currImagePathRef.current)
                 .then((value) => setImageData(value.byteLength > 0 ? value : null))
                 .catch((e) => setImageData(null));
         } else {
             setImageData(null);
         }
-    }, [imageList]);
+    }, [imageIdx]);
 
     const imgBlob =  imageData != null ? new Blob([imageData]) : null;
     const imgURL = imgBlob != null ? URL.createObjectURL(imgBlob) : null;
 
+    const handleLeftBtnClick = () => updateImageIdx(imageIdx <= 0 ? imageIdx : imageIdx - 1);
+    const handleRightBtnClick =
+        () => updateImageIdx(imageIdx < 0 || imageIdx >= imageList.length - 1 ? imageIdx : imageIdx + 1);
+
     return (
         <div className="imageview">
+            <Button onClick={handleLeftBtnClick} className="viewerbtn">
+                <FaCaretLeft/>
+            </Button>
             {imgURL != null ? (<img src={imgURL} alt="my image"/>) : (<></>)}
+            <Button onClick={handleRightBtnClick} className="viewerbtn">
+                <FaCaretRight/>
+            </Button>
         </div>
     );
 }
