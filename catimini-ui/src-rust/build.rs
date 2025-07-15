9805e20f55bdd1f8445332b239d94926d24a36a9
Julien Thierry <julienthierry3110@gmail.com>
2025-07-15 23:16:52 +0200

    [bugfix] catimini: generate icon in png format for linux builds

--- catimini-ui/src-rust/build.rs
+++ catimini-ui/src-rust/build.rs
@@ -1,8 +1,16 @@
-fn check_dummy_icon_exists(icon_path: &str) -> Result<bool, image::ImageError>{
-    if std::fs::exists(icon_path)? {
-        let img = image::ImageReader::open(icon_path)?;
+type RGBABuffer = image::ImageBuffer<image::Rgba<u8>, Vec<u8>>;
+
+fn check_dummy_icon_exists<P>(icon_path: P,
+                              expected_format : &image::ImageFormat) -> Result<bool, image::ImageError>
+where
+    P : AsRef<std::path::Path>
+{
+    if std::fs::exists(&icon_path)? {
+        let img = image::ImageReader::open(&icon_path)?;
         if let Some(fmt) = img.format() {
-            assert!(fmt == image::ImageFormat::Ico, "Expect {icon_path} to have ICO format, got {}", fmt.to_mime_type());
+            assert!(fmt == *expected_format,
+                    "Expect {} to have {} format, got {}",
+                    icon_path.as_ref().display() ,expected_format.to_mime_type(), fmt.to_mime_type());
             return Ok(true)
         }
     }
@@ -10,7 +18,7 @@ fn check_dummy_icon_exists(icon_path: &str) -> Result<bool, image::ImageError>{
     Ok(false)
 }
 
-fn generate_dummy_icon(icon_path: &str) -> Result<(), image::ImageError> {
+fn generate_dummy_icon() -> RGBABuffer {
     let mut img_buf = image::RgbaImage::new(128, 128);
 
     // Generate a 64x64 centered golden square with transparent background
@@ -22,22 +30,41 @@ fn generate_dummy_icon(icon_path: &str) -> Result<(), image::ImageError> {
         }
     }
 
-    let parent_path = std::path::Path::new(icon_path).parent().unwrap();
+    img_buf
+}
+
+fn create_icon_file<P : AsRef<std::path::Path>>(icon_path: P) -> std::io::Result<std::fs::File> {
+    let parent_path = icon_path.as_ref().parent().unwrap();
     if !std::fs::exists(parent_path)? {
         std::fs::create_dir_all(parent_path).unwrap();
     }
-    let ico_file = std::fs::File::options().write(true).create(true).open(icon_path)?;
-    img_buf.write_with_encoder(image::codecs::ico::IcoEncoder::new(ico_file))
+    std::fs::File::options().write(true).create(true).open(icon_path)
 }
 
 fn main() {
     // Tauri requires an icon to exist to create the app.
     // To avoid storing a temporary icon in git, generate a small one at build
     // time when it is needed.
-    let icon_path = "dist/icons/dummy.ico";
 
-    if !check_dummy_icon_exists(icon_path).unwrap() {
-        generate_dummy_icon(icon_path).unwrap();
+    // IconInfo = (path_of_icon, icon_format, write_buf_to_file(file, buf))
+    type IconInfo<'a> = (&'a str, image::ImageFormat, Box<dyn Fn(std::fs::File, &RGBABuffer) -> image::ImageResult<()>>);
+    let icons_info : [IconInfo;2]= [
+        ("dist/icons/dummy.ico", image::ImageFormat::Ico,
+         Box::new(|file, img_buf : &RGBABuffer| {img_buf.write_with_encoder(image::codecs::ico::IcoEncoder::new(file))})),
+        ("dist/icons/dummy.png", image::ImageFormat::Png,
+         Box::new(|file, img_buf : &RGBABuffer| {img_buf.write_with_encoder(image::codecs::png::PngEncoder::new(file))}))
+    ];
+
+    let missing_icons : Vec<&IconInfo> =
+        icons_info.iter().filter(|e| !check_dummy_icon_exists(&e.0, &e.1).unwrap()).collect();
+
+    let icon_buf = if missing_icons.len() > 0  { generate_dummy_icon() } else {
+        return;
+    };
+
+    for icon_info in missing_icons {
+        let icon_file = create_icon_file(&icon_info.0).unwrap();
+        icon_info.2(icon_file, &icon_buf).unwrap();
     }
     tauri_build::build()
 }
