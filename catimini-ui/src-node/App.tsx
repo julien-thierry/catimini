3ff87f849334617ce87c246b1954df9755d1085f
Julien Thierry <julienthierry3110@gmail.com>
2025-07-21 11:25:04 +0200

    [feature] catimini-ui: Add side panel and button to open and close it

--- catimini-ui/src-node/App.tsx
+++ catimini-ui/src-node/App.tsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
+import { Button } from "react-bootstrap";
+import { FaCaretRight, FaCaretDown } from "react-icons/fa";
+
 import "./App.css";
 import ImageViewer from "./ImageViewer";
 
 function App() {
-  return (
-    <main className="container">
-      <h1>Welcome to Catimini</h1>
-      <div className="imgviewercontainer">
-        <ImageViewer/>
-      </div>
-    </main>
+    const [sidePanelOpen, setSidePanelOpen] = useState(false);
+    return (
+        <main>
+            <Button className="sidepanelbtn" onClick={() => setSidePanelOpen(!sidePanelOpen)}>
+                {sidePanelOpen? <FaCaretDown/> : <FaCaretRight/>}
+            </Button>
+            <div className="panelcontainer">
+                { sidePanelOpen ? <div className="sidepanel"/> : <></> }
+                <div className="imagepanel">
+                    <h1>Welcome to Catimini</h1>
+                    <div className="imgviewercontainer">
+                        <ImageViewer/>
+                    </div>
+                </div>
+            </div>
+        </main>
   );
 }
 
