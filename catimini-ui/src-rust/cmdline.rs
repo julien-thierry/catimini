2188b9a863e70472b54c3feea8b831b2c29369fd
Julien Thierry <julienthierry3110@gmail.com>
2025-08-04 14:17:46 +0200

    [feature] catimini-ui: Add option to open front-end debug console

--- catimini-ui/src-rust/cmdline.rs
+++ catimini-ui/src-rust/cmdline.rs
@@ -6,6 +6,10 @@ pub struct CLIArgs {
     // Gallery root directories
     #[arg(short='d', long="root-dir", value_name="DIR")]
     pub root_directories: Vec<std::path::PathBuf>,
+
+    #[cfg(debug_assertions)]
+    #[arg(long="debug-front", help="Open debug console on starting front-end", default_value_t=false)]
+    pub debug_front : bool,
 }
 
 pub fn get_args() -> CLIArgs {
