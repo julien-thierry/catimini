49ed683aa577e323e86e2a4ef681a789508f38ea
Julien Thierry <julienthierry3110@gmail.com>
2025-08-04 14:17:46 +0200

    [feature] catimini-ui: Add option to open front-end debug console

--- catimini-ui/src-rust/main.rs
+++ catimini-ui/src-rust/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 mod cmdline;
 
 fn main() {
@@ -9,7 +11,18 @@ fn main() {
     let root_directories = if !args.root_directories.is_empty() { args.root_directories }
                            else { vec![std::env::current_dir().unwrap_or_default()] };
 
+    #[cfg(debug_assertions)]
+    let debug_front = args.debug_front;
+
     catimini_tauri::init(&root_directories)
+        .setup(move |app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            if debug_front {
+                let window = app.get_webview_window("main").unwrap();
+                window.open_devtools();
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
